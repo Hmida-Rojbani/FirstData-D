e7783dc21427783af7b21866b187bdfe5453786f
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2020-10-27 10:26:32 +0100

    update

--- src/main/java/de/tekup/rest/data/models/PersonEntity.java
+++ src/main/java/de/tekup/rest/data/models/PersonEntity.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -26,7 +27,8 @@ public class PersonEntity {
 	
 	private LocalDate dateOfBirth;
 	
-	private String address;
+	@OneToOne
+	private AddressEntity address;
 	
 
 }
