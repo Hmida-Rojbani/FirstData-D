121de7f89bc739b211fbd00396d9761d8a6b7d72
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2020-10-27 09:24:51 +0100

    first commit

---
+++ src/main/java/de/tekup/rest/data/services/PersonService.java
@@ -0,0 +1,16 @@
+package de.tekup.rest.data.services;
+
+import java.util.List;
+
+import de.tekup.rest.data.models.PersonEntity;
+
+public interface PersonService {
+	
+	List<PersonEntity> getAllEntities();
+	PersonEntity getEntityById(long id);
+	PersonEntity createEntity(PersonEntity entity);
+	PersonEntity modifyEntity(long id, PersonEntity newEntity);
+	PersonEntity deleteEntity(long id);
+	
+
+}
